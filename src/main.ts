f222af88da1987f53403c2befbfc24849fb9ee4e
Long Pham <longkul1478@gmail.com>
2023-09-25 13:30:11 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ declare const module: any;
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use('/static', express.static('public'));
-  await app.listen(4000);
+  await app.listen(process.env.APP_PORT || 4000);
 
   if (module.hot) {
     module.hot.accept();
